beea8439a090158d17a6306f6c1c70e3661aa9c0
Tome Nelson <tomenelson@gmail.com>
2017-04-29 22:34:59 +0300

    More tree interface

--- wet1/avl.h
+++ wet1/avl.h
@@ -1,19 +1,75 @@
 #ifndef AVL_H
 #define AVL_H
 
-template <class Value> class Node {
-  int key;
-  Value* value;
-  Node* parent;
-  Node* left;
-  Node* right;
+#include <iostream>
+
+
+template <class Key, class Value>
+class AVLTree {
+
+private:
+  class Node;
 
 public:
-  Node(int key, Value* value): key(key), value(value), parent(nullptr), left(nullptr), right(nullptr) {};
-};
+  /*
+    Class assumes the key of the tree has overloaded operators: >,<,==,!=
+   */
+  class Iterator{
+  private:
+    Node* current;
 
-template <class Value> class AVLTree {
-  Node<Value> head;
+  public:
+    Value& operator*();
+    Iterator operator++();
+    Iterator operator--();
+    bool operator==(Iterator& second);
+    bool operator!=(Iterator& second);
+    void repalceWith(Key& key, Value& value);
+  };
+
+private:
+  class Node {
+  private:
+    friend void Iterator::repalceWith(Key& key, Value& value);
+    Key* key;
+    Value* value;
+    Node* parent;
+    Node* left;
+    Node* right;
+    int BF;
+    int hightRight;
+    int hightLeft;
+  public:
+
+    Node(Key& key, Value& value, Node* parent=NULL): key(key), value(value), left(NULL), right(NULL), BF(0), hightRight(0), hightLeft(0), parent(parent){};
+  };
+
+  Node head;
+
+  AVLTree(AVLTree& tree);
+  static void rollRR();
+  static void rollLL();
+  static void rollRL();
+  static void rollLR();
+  static void balance();
+
+  //debug functions
+  static bool checkBF();
+  static bool checkOrder();
+
+public:
+  AVLTree();
+  ~AVLTree();
+
+
+  Value& find(Key key);
+
+  void insert(Key& key, Value& value);
+  void remove(Key& key, Value& value);
+
+  Iterator first();
+  Iterator last();
+  Iterator end();
 
 };
 #endif
