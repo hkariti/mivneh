5da2a4bde68e07994fa05e91ea8ec1146f908212
Tome Nelson <tomenelson@gmail.com>
2017-05-01 19:35:09 +0300

    t2

--- wet1/avlTest.cpp
+++ wet1/avlTest.cpp
@@ -11,7 +11,7 @@ void AVLTreeRollTest(){
   std::cout << "right: " << *(((tree.head)->right)->key) << std::endl;
   std::cout << "left: " << *(((tree.head)->left)->key) << std::endl;
 
-  //rolll LL test
+  //roll LL test
   AVLTree<int, int> tree2;
   tree2.insert(2,3);
   tree2.insert(6,7);
@@ -20,6 +20,24 @@ void AVLTreeRollTest(){
   std::cout << "head: " << *((tree2.head)->key) << std::endl;
   std::cout << "right: " << *(((tree2.head)->right)->key) << std::endl;
   std::cout << "left: " << *(((tree2.head)->left)->key) << std::endl;
+
+  //roll RL test
+  AVLTree<int, int> tree3;
+  tree3.insert(1,3);
+  tree3.insert(5,7);
+  tree3.insert(3,9);
+  std::cout << "head: " << *((tree3.head)->key) << std::endl;
+  std::cout << "right: " << *(((tree3.head)->right)->key) << std::endl;
+  std::cout << "left: " << *(((tree3.head)->left)->key) << std::endl;
+
+  //roll LR test
+  AVLTree<int, int> tree4;
+  tree4.insert(3,3);
+  tree4.insert(1,7);
+  tree4.insert(2,9);
+  std::cout << "head: " << *((tree4.head)->key) << std::endl;
+  std::cout << "right: " << *(((tree4.head)->right)->key) << std::endl;
+  std::cout << "left: " << *(((tree4.head)->left)->key) << std::endl;
 
 }
 
