c835030d43badd8e9cb9e5afaf8a44c34faad62a
Tome Nelson <tomenelson@gmail.com>
2017-05-02 19:50:54 +0300

    added class pair + test

---
+++ wet1/pair.cpp
@@ -0,0 +1,51 @@
+#include "pair.h"
+#include <iostream>
+
+PairTest(){
+
+  Pair first(1,1);
+  Pair copy(first);
+  if(copy == first){
+    std::cout << "copy success" << std::endl;
+  }
+
+  Pair second(1,2);
+  if(second == first){
+    std::cout << "ABORT (equal)" << std::endl;
+  }
+  if(first > second){
+    std::cout << "ABORT (>)" << std::endl;
+  }
+  if(second > first){
+    std::cout << "success (>)" << std::endl;
+  }
+  if(first < second){
+    std::cout << "success (<)" << std::endl;
+  }
+  if(second < first){
+    std::cout << "ABORT (<)" << std::endl;
+  }
+
+  Pair third(2,1);
+  if(second != third){
+    std::cout << "success (!=)" << std::endl;
+  }
+  if(third == first){
+    std::cout << "ABORT (==)" << std::endl;
+  }
+  if(third < second){
+    std::cout << "ABORT (<)" << std::endl;
+  }
+  if(first > third){
+    std::cout << "ABORT (>)" << std::endl;
+  }
+
+}
+
+
+int main(){
+  PairTest();
+  return 0;
+}
+
+
---
+++ wet1/pair.h
@@ -0,0 +1,56 @@
+#ifndef PAIR_H
+#define PAIR_H
+
+
+class Pair{
+
+  int power;
+  int id;
+
+public:
+  Pair(const int& power, const int& id) : power(power), id(id) {}
+  ~Pair() {}
+  Pair(const Pair& pair) : power(pair.power), id(pair.id) {}
+
+  bool operator==(const Pair& pair) const{
+    if(this->power == pair.power && this->id == pair.id){
+      return true;
+    }
+    return false;
+  }
+  bool operator!=(const Pair& pair) const{
+    if(*this == pair){
+      return false;
+    }
+    return true;
+  }
+  bool operator>(const Pair& pair) const{
+    if(this->power > pair.power){
+      return true;
+    }
+    else if(this->power == pair.power){
+      if(this->id > pair.id){
+        return true;
+      }
+    }
+    return false;
+  }
+  bool operator<(const Pair& pair) const{
+    if(this->power < pair.power){
+      return true;
+    }
+    else if(this->power == pair.power){
+      if(this->id < pair.id){
+        return true;
+      }
+    }
+    return false;
+  }
+
+
+
+
+};
+
+
+#endif
