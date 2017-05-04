e4ee12e4adc9af0b213504ffac7f6782356e8ed9
Tome Nelson <tomenelson@gmail.com>
2017-05-04 20:07:27 +0300

    fixed small details for student, renamed pair file

--- wet1/student.h
+++ wet1/student.h
@@ -5,20 +5,29 @@ class Student{
   int id;
   const int grade;
   int power;
+  Team* team;
 
 public:
   Student(const int& id, const int& grade, const int& power) : id(id), grade(grade), power(power) {}
   ~Student() {}
-  Student (const Student& student) : id(student.id), grade(student.grade), power(student.power) {}
-
+  
   const int& ID() const{
     return this->id;
   }
   const int& Grade() const{
     return this->grade;
   }
-  void update(int addition) {
+  void updatePower(int addition) {
     this->power = this->power + addition;
+  }
+  const int& getPower() const{
+    return power;
+  }
+  const Team* getTeam() const{
+    return team;
+  }
+  void updateTeam(const Team& newTeam) {
+    this->team = newTeam;
   }
 
 };
