00a6035346891ce11f8791d2492086518cecd985
Tome Nelson <tomenelson@gmail.com>
2017-05-05 18:29:01 +0300

    school.h written but not tested

--- wet1/school.h
+++ wet1/school.h
@@ -22,6 +22,8 @@ public:
     if(studentID <= 0 || power <= 0 || grade < 0){
       return INVALID_INPUT;
     }
+
+    //add student to student tree
     try{
       students.insert(studentID, new Student(studentID, grade, power));
     }
@@ -31,6 +33,8 @@ public:
     catch(std::bad_alloc& e){
       return ALLOCATION_ERROR;
     }
+
+    //add student to the globalTeam
     try {
       globalTeam.addStudent(students.find(studentID));
     }
@@ -44,6 +48,8 @@ public:
     if(teamID <= 0){
       return INVALID_INPUT;
     }
+
+    //add team to team tree
     try{
       Team* newTeam = new Team(teamID);
       teams.insert(teamID, newTeam);
@@ -61,6 +67,8 @@ public:
     if(studentID <= 0 || teamID <= 0){
       return INVALID_INPUT;
     }
+
+    //check that the student and the team exist in the system
     try{
       students.find(studentID);
       teams.find(teamID);
@@ -78,21 +86,150 @@ public:
       return FAILURE;
     }
 
-    Student* currentStudent = students.find(studentID);
+    //remove student from previous team and add to the current team.
+    try{
+      Student* currentStudent = students.find(studentID);
     Team* prevTeam = currentStudent->getTeam();
-    prevTeam->removeStudent(currentStudent->getPower(), studentID);
+    if(prevTeam != NULL){
+      prevTeam->removeStudent(currentStudent->getPower(), studentID);
+    }
     currentStudent->updateTeam(*teams.find(teamID));
-
-
-
+    teams.find(teamID)->addStudent(currentStudent);
+    }
+    catch(std::bad_alloc& e){
+      return ALLOCATION_ERROR;
+    }
   }
 
-  StatusType getMostPowerful(int teamID, int* studentID);
+  StatusType getMostPowerful(int teamID, int* studentID){
+    if(teamID == 0) || studentID == NULL){
+      return INVALID_INPUT;
+    }
 
-  StatusType removeStudent(int studentID);
+  //if we are looking for the most powerful student in the whole system
+    if(teamID < 0){
+      *studentID = globalTeam.getMostPowerfulStudent();
+      return SUCCESS;
+    }
 
-  StatusType getAllStudentsByPower(int teamID, int** students, int* numOfStudents);
+  //if we are looking for the most powerful student in a certain team
+    else{
+      //check that the team exists in the system
+      try{
+        teams.find(teamID);
+      }
+      catch(teamTree::NotFound& e){
+        return FAILURE;
+      }
+      catch(teamTree::isEmpty& e){
+        return FAILURE;
+      }
+      *studentID = teams.find(teamID)->getMostPowerfulStudent();
+      return SUCCESS;
+    }
+    }
 
-  StatusType increaseLevel(int grade, int powerIncrease);
+  StatusType removeStudent(int studentID){
+    if(studentID <= 0){
+      return INVALID_INPUT;
+    }
+
+    //checks that the student exists in the system
+    try{
+      students.find(studentID);
+    }
+    catch(idTree::NotFound& e){
+      return FAILURE;
+    }
+    catch(idTree::isEmpty& e){
+      return FAILURE;
+    }
+
+    //remove the student from his team, from globalTeam and from the id tree
+    Student* student = students.find(studentID);
+    globalTeam.removeStudent(student->getPower(), studentID);
+    if(student->getTeam() != NULL){
+      student->getTeam()->removeStudent(student->getPower(), studentID);
+    }
+    students.remove(studentID);
+    delete student;
+
+    return SUCCESS;
+  }
+
+  StatusType getAllStudentsByPower(int teamID, int** students, int* numOfStudents){
+    if(students == NULL || numOfStudents == NULL ||teamID == 0){
+      return INVALID_INPUT;
+    }
+
+    //if we are looking for all the students in the system
+    if(teamID < 0){
+      try{
+        *students = globalTeam.getAllStudentsByPower(numOfStudents);
+        return SUCCESS;
+      }
+      catch(std::bad_alloc& e){
+        return ALLOCATION_ERROR;
+      }
+    }
+
+    //if we are lookig for all the students in a certian team
+    else{
+      try{
+        teams.find(teamID);
+      }
+      catch(teamTree::NotFound& e){
+        return FAILURE;
+      }
+      catch(teamTree::isEmpty& e){
+        return FAILURE;
+      }
+      try {
+      *students = teams.find(teamID)->getAllStudentsByPower(numOfStudents);
+      return SUCCESS;
+      }
+      catch(std::bad_alloc& e){
+        return ALLOCATION_ERROR;
+      }
+    }
+  }
+
+StatusType increaseLevel(int grade, int powerIncrease){
+  if(grade < 0 || powerIncrease <= 0){
+    return INVALID_INPUT;
+  }
+
+  try {
+    //update only happens in the globalTeam because that is the place where we have pointers to all the students in the system. 
+    globalTeam.increaseLevel(grade, powerIncrease);
+    //for each team the increase happens with 0 because the students power was already increased in the globalTeam, but we still need to re-arrange them in the teams tree.
+    teamTree::Iterator it = teams.first();
+  while(it != teams.end()){
+    it.value()->increaseLevel(grade, 0);
+    it++;
+  }
+  }
+  catch(std::bad_alloc& e){
+    return ALLOCATION_ERROR;
+  }
+  return SUCCESS;
+}
+
+~School(){
+  //free all of the memory taken up by students
+  idTree::Iterator itID = students.first();
+  while(itID != students.end()){
+    delete itID.value();
+    itID++;
+  }
+  //free all the memory taken up by the teams
+  teamTree::Iterator itTeam = teams.first();
+  while(itTeam != teams.end()){
+    delete itTeam.value();
+    itTeam++;
+  }
+}
+
 };
+
 #endif
