a0f0c36855d96523e13f59dc6698134c7a80ac0f
Hagai Kariti <hkariti@gmail.com>
2017-05-30 18:13:39 +0300

    small fixes in testUtils

--- wet2/testUtils.h
+++ wet2/testUtils.h
@@ -1,10 +1,11 @@
 #include <exception>
 #include <string>
+#include <iostream>
 
 #define Q(x) #x
 #define THROWS(expr, excep) try { expr; throw ExpectedException("expected " Q(excep)); } catch (excep& e) {}
 
-#define RUN_TEST(test) try { test(); } catch (std::exception& e) { std::cout << "Test " Q(test) " failed: " << e.what() << std::endl; }
+#define RUN_TEST(test) try { test(); std::cout << "Test " Q(test) " succeeded" << std::endl; } catch (std::exception& e) { std::cout << "Test " Q(test) " failed: " << e.what() << std::endl; }
 class ExpectedException : public std::exception {
   const std::string msg;
 public:
