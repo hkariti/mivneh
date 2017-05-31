8665aa47f933a8db9437fcc5f67cb07fd5d52fd8
Tome Nelson <tomenelson@gmail.com>
2017-05-31 20:59:37 +0300

    another commit after weird things

--- wet2/avl.h
+++ wet2/avl.h
@@ -376,8 +376,8 @@ std::cout << "parent error" << std::endl;
     checkRankRecurse(head);
   }
 
-  static void buildEmptyRecursive(Node* head, int n){
-    if(n == 1) return;
+  static Node* buildEmptyRecursive(Node* head, int n){
+    if(n == 1) return NULL;
     head->left = new Node(head);
     head->right = new Node(head);
 
