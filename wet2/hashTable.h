3e8bb65085004a24dfcbbac4d3b1d314fe34547a
Hagai Kariti <hkariti@gmail.com>
2017-06-06 11:52:51 +0300

    Hashtable fixes
    - Make HashTableEntry a private class inside HashTable
    - Fix resize() invalidating entry pointers
    - Fix infinite loop in searchFull()
    - Fix wrong entry being returned in insert() if resize() needs to happen

--- wet2/hashTable.h
+++ wet2/hashTable.h
@@ -3,42 +3,53 @@
 
 static const int INIT_SIZE = 32;
 
-template <class Key, class Value> class HashTableEntry {
-private:
-  enum entryState { free, deleted, full };
-  entryState state;
-  Key key;
-  Value* value;
-public:
-  HashTableEntry() : state(free), value(NULL) {};
-  ~HashTableEntry() {
-    delete value;
-  }
-  bool isFree() { return state == free; }
-  bool isFull() { return state == full; }
-  bool isDeleted() { return state == deleted; }
-  // Init the entry with the given key and an empty value
-  void init(const Key& new_key, const Value& new_value) {
-    key = new_key;
-    state = full;
-    if (value) delete value;
-    value = new Value(new_value);
-  }
-  const Key& getKey() const { return key; };
-  Value& getValue() { return *value; }
-  void markDeleted() { state = deleted; }
-};
 
 template <class Key, class Value> class HashTable {
 private:
-  typedef HashTableEntry<Key, Value> TableEntry;
+  class HashTableEntry {
+  private:
+    enum entryState { free, deleted, full };
+    entryState state;
+    Key key;
+    Value* value;
+  public:
+    class EntryAlreadyFull : public std::exception {};
+    HashTableEntry() : state(free), value(NULL) {};
+    ~HashTableEntry() {
+      if (state == full) delete value;
+    }
+    bool isFree() { return state == free; }
+    bool isFull() { return state == full; }
+    bool isDeleted() { return state == deleted; }
+    // Init the entry with the given key and value
+    void init(const Key& new_key, const Value& new_value) {
+      if (state == full) throw EntryAlreadyFull();
+      value = new Value(new_value);
+      key = new_key;
+      state = full;
+    }
+    const Key& getKey() const { return key; };
+    Value& getValue() { return *value; }
+    void clearEntry() {
+      if (value) {
+        delete value;
+        value = NULL;
+      }
+      state = deleted;
+    }
+    // Only mark entry as deleted without freeing the value
+    // Dangerous! Used by hash.resize() only
+    void markDeleted() {
+      state = deleted;
+    }
+  };
   int size, used;
-  TableEntry *array;
+  HashTableEntry *array;
 public:
   class NoSuchEntry : public std::exception {};
   class EntryExists : public std::exception {};
   HashTable() : size(INIT_SIZE), used(0) {
-    array = new TableEntry[size];
+    array = new HashTableEntry[size];
   }
 
   ~HashTable() {
@@ -56,7 +67,7 @@ public:
       array[i].init(key, value);
       used++;
       if (used == size) resize(2*size);
-      return array[i].getValue();
+      return (*this)[key];
     }
   }
 
@@ -72,7 +83,7 @@ public:
 
   void remove(const Key& key) {
     int i = searchFull(key);
-    array[i].markDeleted();
+    array[i].clearEntry();
     used--;
     if (used <= size/4) resize(size/2);
   }
@@ -91,16 +102,17 @@ private:
   void resize(int new_size) {
     if (new_size < INIT_SIZE) return;
 
-    TableEntry *old_array = array;
+    HashTableEntry *old_array = array;
     int old_size = size;
-    array = new TableEntry[new_size];
+    array = new HashTableEntry[new_size];
     size = new_size;
-    used = 0;
     // Copy all full elements from old array, rehashing them
     for (int i = 0; i < old_size; i++) {
       if (!old_array[i].isFull()) continue;
       Key key = old_array[i].getKey();
-      insert(key, old_array[i].getValue());
+      int new_index = searchEmpty(key);
+      array[new_index] = old_array[i];
+      old_array[i].markDeleted();
     }
     delete[] old_array;
   }
@@ -113,6 +125,7 @@ private:
       if (array[index].isFull() && array[index].getKey() == key)
         return index;
       step++;
+      if (step > size) break;
     } while (!array[index].isFree());
     throw NoSuchEntry();
   }
