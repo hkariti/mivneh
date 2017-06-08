5b54bd7dcc52c02e4cf1b6928bd1eef6adb9273f
Hagai Kariti <hkariti@gmail.com>
2017-06-08 18:20:03 +0300

    wet1: retro input checks. fuck.

--- wet1/library1.cpp
+++ wet1/library1.cpp
@@ -6,34 +6,42 @@ void* Init() {
 }
 
 StatusType AddStudent(void *DS, int StudentID, int Grade, int Power) {
+    if (!DS) return INVALID_INPUT;
     return ((School*)DS)->addStudent(StudentID, Grade, Power);
 }
 
 StatusType AddTeam(void *DS, int TeamID) {
+    if (!DS) return INVALID_INPUT;
     return ((School*)DS)->addTeam(TeamID);
 }
 
 StatusType MoveStudentToTeam(void *DS, int StudentID, int TeamID) {
+    if (!DS) return INVALID_INPUT;
     return ((School*)DS)->moveStudentToTeam(StudentID, TeamID);
 }
 
 StatusType GetMostPowerful(void *DS, int TeamID, int *StudentID) {
+    if (!DS) return INVALID_INPUT;
     return ((School*)DS)->getMostPowerful(TeamID, StudentID);
 }
 
 StatusType RemoveStudent(void *DS, int StudentID) {
+    if (!DS) return INVALID_INPUT;
     return ((School*)DS)->removeStudent(StudentID);
 }
 
 StatusType GetAllStudentsByPower(void *DS, int TeamID, int **Students, int *numOfStudents) {
+    if (!DS) return INVALID_INPUT;
     return ((School*)DS)->getAllStudentsByPower(TeamID, Students, numOfStudents);
 }
 
 StatusType IncreaseLevel(void *DS, int Grade, int PowerIncrease) {
+    if (!DS) return INVALID_INPUT;
     return ((School*)DS)->increaseLevel(Grade, PowerIncrease);
 }
 
 void Quit(void** DS) {
+    if (!DS) return;
     delete (School*)(*DS);
     *DS = NULL;
 }
